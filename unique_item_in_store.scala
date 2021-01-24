33e225853906d4299eaf1adadb1a61f9cc7fa857
muthu84 <69139706+muthu84@users.noreply.github.com>
2021-01-24 10:31:25 +0530

    Create unique_item_in_store.scala

---
+++ unique_item_in_store.scala
@@ -0,0 +1,25 @@
+/*
+From the below file print the unique item and store name:
+
+retailer.txt:
+
+Sears,shoe,ring,pan,shirt
+Walamrt,ring,pan,hat,meat
+Target,shoe,pan,shirt,hat
+
+*/
+
+Code:
+
+>spark-shell
+scala>var uniqueItem=""
+scala> var storeName=""
+scala> val rdd1 = sc.textFile("file://home/user1/retailer.txt")
+scala> rdd1.map(x=>x.split(",")).map(x=>Array(x(1),x(2),x(3),x(4))).flatMap(x=>x).map(x=>(x,1)).reduceByKey(_+_).collect().foreach(x=>if(x._2==1)uniqueItem=x._1)
+scala> rdd1.map(x=>x.split(",")).map(x=>(x(0),x(1),x(2),x(3),x(4))).collect().foreach(x=>if((x._2==uniqueItem)||(x._3==uniqueItem)||(x._4==uniqueItem)||(x._5==uniqueItem))storeName=x._1)
+scala> println("Store Name " + storeName)
+scala> println("Unique Item " + uniqueItem)
+
+Output:
+Store Name Walamrt
+UNIQUE ITEM meat
