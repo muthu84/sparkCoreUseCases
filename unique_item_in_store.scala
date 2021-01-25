f52de7581f8ba4009c75ba89e5ce5dd84fb311a2
muthu84 <69139706+muthu84@users.noreply.github.com>
2021-01-25 20:27:55 +0530

    Update unique_item_in_store.scala

--- unique_item_in_store.scala
+++ unique_item_in_store.scala
@@ -23,3 +23,24 @@ scala> println("Unique Item " + uniqueItem)
 Output:
 Store Name Walmart
 UNIQUE ITEM meat
+
+/*
+one more approach
+*/
+
+val data = """   
+Sears, shoe, ring, pan, shirt
+Walmart, ring, pan, hat, meat
+Target, shoe, pan, shirt, hat
+  """.trim
+
+val records = sc.parallelize(data.split('\n'))
+	
+val r2 = records.flatMap { r =>
+      val Array(company, product1, product2, product3, product4) = r.replaceAll(" +", "").split(',');
+      List(((company, product1), 1), ((company, product2), 1), ((company, product3), 1), ((company, product4), 1))
+    }
+val r3 = r2.map { case ((company, product), total) => ((product, total), company) }
+val r4 = r3.reduceByKey{ (x, y) => (x + " " + y) }.map(case ((items),stores) => ((items),Array(stores)))
+val r5 = r4.map{case ((items),stores) => ((items),stores.split(" "))}
+val r6 = r5.map{x=>x}.filter(x=>x._2.length==1).collect
